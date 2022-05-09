749d03c0b2c81e0a6cd354be009f0900e84774d0
AsadAhmad1453 <94599676+AsadAhmad1453@users.noreply.github.com>
2022-05-10 02:46:35 +0500

    update

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -27,4 +27,16 @@ $('.carousel').carousel({
   }
 });
 
-  
+  $('.menu-toggle').click(function() {
+
+    /*--------------------------------------------------------------*/
+    /*open-close Navigation menu btn, 500 milliseconds to drop-close*/
+    /*--------------------------------------------------------------*/
+    $('.site-nav').toggleClass('site-nav-open', 500);
+        
+    /*-------------------------------------------------------------*/
+    /* changes hamburger to red X for close button .open CSS       */
+    /*-------------------------------------------------------------*/
+    $(this).toggleClass('open');
+
+  })
